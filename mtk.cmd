0999717efa3f01444a1b05daa787709b4be2f241
Jairah7 <116828303+Jairah7@users.noreply.github.com>
2023-04-29 15:24:06 +0800

    Update mtk.cmd

--- mtk.cmd
+++ mtk.cmd
@@ -17,7 +17,7 @@ goto exit
 
 :smtk
 ::safeformat
-if exist testpoint start ffplay -i %tp% -window_title "%search%" by drox-PH-Ceb
+if exist testpoint start ffplay -i %tp% -window_title "%search%"
 echo. >>logs.txt
 echo *** %date%-%time% Removing password and FRP *** >>logs.txt
 %mtk_process% w para,frp mtk\password.dll,mtk\frp.exe >>logs.txt
