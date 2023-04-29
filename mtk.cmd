89a4427106f68271f257271326dca2acba8e0c11
Jairah7 <116828303+Jairah7@users.noreply.github.com>
2023-04-29 13:47:46 +0800

    Update mtk.cmd

--- mtk.cmd
+++ mtk.cmd
@@ -19,7 +19,7 @@ goto exit
 ::safeformat
 echo dir
 pause
-if exist testpoint ffplay "%tp%" -vf "crop=in_w:in_h-40,scale=720:480" -window_title "%search%" by drox-PH-Ceb
+if exist testpoint ffplay -i "%tp%" -vf "crop=in_w:in_h-40,scale=720:480" -window_title %search% by drox-PH-Ceb
 if exist frp.txt goto frp
 echo. >>logs.txt
 echo *** %date%-%time% Removing password and FRP *** >>logs.txt
