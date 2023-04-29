ebc3601329b9e460990d2707daa9695a4222d324
Jairah7 <116828303+Jairah7@users.noreply.github.com>
2023-04-29 18:13:40 +0800

    Update models.cmd

--- models.cmd
+++ models.cmd
@@ -2,6 +2,8 @@ if exist logs.txt del logs.txt
 echo Created by drox-PH-Ceb for script just contact https://web.facebook.com/jairah.mazo.5/ >%fb%
 echo Created by drox-PH-Ceb for script just contact jairahmazo@gmail.com >%yt%
 ::Manual Referrences
+echo "%search%" | findstr /i "AndroidUtility" && set "url=https://www.mediafire.com/file/m8mok08ashdttxe/AndroidUtility.v104.7z/file" && goto software
+echo "%search%" | findstr /i "Samfw" && set "url=https://www.mediafire.com/file/upaiwadhmstm6js/SamFwToolSetup_v4.6.zip/file" && goto software
 echo "%search%" | findstr /i "C11" && set "url=https://www.youtube.com/watch?v=q5o6-6KnGCw&pp=ygUOYzExIGZycCBieXBhc3M%3D" && goto found 
 echo "%search%" | findstr /i "y15s" && set "url=https://www.youtube.com/watch?v=dvA0Karr9e4" && goto found
 echo "%search%" | findstr /i "y15a" && set "url=https://www.youtube.com/watch?v=dvA0Karr9e4" && goto found
@@ -28,3 +30,8 @@ exit /b
 :msg
 echo  "%search%" model is suported. Just Hold Vol_up+Vol_down and insert usb cable >logs.txt
 exit /b
+
+:software
+echo  "%search%" software found Please wait for link..>logs.txt
+start "" "%url%"
+exit
