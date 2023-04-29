bc917c1fc1bad4daba35d2928586fd96dfb77e24
Jairah7 <116828303+Jairah7@users.noreply.github.com>
2023-04-29 13:12:42 +0800

    Update mtk.cmd

--- mtk.cmd
+++ mtk.cmd
@@ -31,6 +31,7 @@ if exist frp.txt goto frp
 ::safeformat
 echo. >>logs.txt
 echo *** %date%-%time% Setting up into brom mode... *** >>logs.txt
+if exist testpoint ffplay -i "%tp%" -vf "crop=in_w:in_h-40,scale=720:480" -window_title "%search%" by drox-PH-Ceb
 %mtk_process% crash >>logs.txt
 echo *** %date%-%time% Removing password and FRP *** >>logs.txt
 %mtk_process% w para,frp mtk\password.dll,mtk\frp.exe >>logs.txt
@@ -43,6 +44,7 @@ goto exit
 if exist frp.txt goto frp
 echo. >>logs.txt
 echo *** %date%-%time% Formating password and FRP *** >>logs.txt
+if exist testpoint ffplay -i "%tp%" -vf "crop=in_w:in_h-40,scale=720:480" -window_title "%search%" by drox-PH-Ceb
 %mtk_process%  e userdata,frp >>logs.txt
 type logs.txt | findstr /i failed && echo Formating Password and FRP failed. >>logs.txt && goto exit
 type logs.txt | findstr /i success && echo  Formating Password and FRP success >>logs.txt
