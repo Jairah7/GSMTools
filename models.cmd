df772c65c1203d1b6bece6784657b78886cb53f4
Jairah7 <116828303+Jairah7@users.noreply.github.com>
2023-04-29 17:47:24 +0800

    Update models.cmd

--- models.cmd
+++ models.cmd
@@ -2,14 +2,14 @@ if exist logs.txt del logs.txt
 echo Created by drox-PH-Ceb for script just contact https://web.facebook.com/jairah.mazo.5/ >%fb%
 echo Created by drox-PH-Ceb for script just contact jairahmazo@gmail.com >%yt%
 ::Manual Referrences
-echo "%search%" | findstr /i "C11" && set "url=https://www.youtube.com/watch?v=q5o6-6KnGCw&pp=ygUOYzExIGZycCBieXBhc3M%3D" &&goto found 
-echo "%search%" | findstr /i "y15s" && set "url=https://www.youtube.com/watch?v=dvA0Karr9e4" &&goto found
-echo "%search%" | findstr /i "y15a" && set "url=https://www.youtube.com/watch?v=dvA0Karr9e4" &&goto found
+echo "%search%" | findstr /i "C11" && set "url=https://www.youtube.com/watch?v=q5o6-6KnGCw&pp=ygUOYzExIGZycCBieXBhc3M%3D" && goto found 
+echo "%search%" | findstr /i "y15s" && set "url=https://www.youtube.com/watch?v=dvA0Karr9e4" && goto found
+echo "%search%" | findstr /i "y15a" && set "url=https://www.youtube.com/watch?v=dvA0Karr9e4" && goto found
 echo "%search%" | findstr /i "A20s" && set "url=https://www.youtube.com/watch?v=J8h-IK4u9SM" && goto found
 echo "%search%" | findstr /i "A30s" && set "url=https://www.youtube.com/watch?v=J8h-IK4u9SM" && goto found
 echo "%search%" | findstr /i "xiaomi 8t" && set "url=https://www.youtube.com/watch?v=16_Te9jvrdw&pp=ygUKeGlhb21pIGZycA%3D%3D" && goto found
 echo "%search%" | findstr /i "y15" && set "url=https://www.youtube.com/watch?v=dvA0Karr9e4" && goto found
-echo "%search%" | findstr /i "infinix" && set "url=https://www.youtube.com/watch?v=ktv8_wty2XY&pp=ygULaW5maW5peCBmcnA%3D" &&goto found
+echo "%search%" | findstr /i "infinix" && set "url=https://www.youtube.com/watch?v=ktv8_wty2XY&pp=ygULaW5maW5peCBmcnA%3D" && goto found
 ::-----------------------------------------------------------------------------------------------------------------------------------------------
 ::direct process
 echo "%search%" | findstr /i "gui" && echo  "%search%" Launching MTK Gui... >logs.txt && call mtk\mtkgui.bat && exit
