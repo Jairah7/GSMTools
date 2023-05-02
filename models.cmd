9ba4a2b49ec9668f2979ec43a4f8380e485182e8
Jairah7 <116828303+Jairah7@users.noreply.github.com>
2023-05-02 22:14:08 +0800

    Update models.cmd

--- models.cmd
+++ models.cmd
@@ -61,6 +61,7 @@ goto exit
 :audio
 if not exist "C:\Users\%username%\Desktop\audio" mkdir "C:\Users\%username%\Desktop\audio"
 plugins\yt.exe -i -x -c -w --no-warnings --audio-format mp3 --geo-bypass --yes-playlist --audio-quality 5 -o "C:\Users\%username%\Desktop\audio\%%(title)s.%%(ext)s" "%search%" >>logs.txt
+echo downloading...finished. >>logs.txt
 goto exit
 
 :exit 
