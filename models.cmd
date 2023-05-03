af73f8d664d39c753904c1d949e528f704858d2c
Jairah7 <116828303+Jairah7@users.noreply.github.com>
2023-05-04 00:58:25 +0800

    Update models.cmd

--- models.cmd
+++ models.cmd
@@ -13,18 +13,18 @@ echo "%search%" | findstr /i "eFixer" && set "url=https://www.mediafire.com/file
 ::Manual Referrences
 echo "%search%" | findstr /i "oppo_A5_2020" && set "url=https://www.youtube.com/watch?v=HVEvPXsgKss&t=84s" && goto found 
 echo "%search%" | findstr /i "infinix_smart_5" && set "url=https://www.youtube.com/watch?v=y8kSKE16UqM" && goto found
-echo "%search%" | findstr /i "y12s" && set "url=https://www.youtube.com/watch?v=QynCJz9jJE8" && goto found
+echo "%search%" | findstr /i "vivo_y12s" && set "url=https://www.youtube.com/watch?v=QynCJz9jJE8" && goto found
 echo "%search%" | findstr /i "reno_5" && set "url=https://www.youtube.com/watch?v=oKGSOWLqcTk" && goto found
-echo "%search%" | findstr /i "y12" && set "url=https://www.youtube.com/watch?v=vLApPHWV8rU" && goto found
-echo "%search%" | findstr /i "y91i" && set "url=https://www.youtube.com/watch?v=SSDjMrO696Y&t=4s" && goto found
-echo "%search%" | findstr /i "Y35" && set "url=https://www.youtube.com/watch?v=-8WFw32moKA" && goto found
-echo "%search%" | findstr /i "C11" && set "url=https://www.youtube.com/watch?v=q5o6-6KnGCw&pp=ygUOYzExIGZycCBieXBhc3M%3D" && goto found 
-echo "%search%" | findstr /i "y15s" && set "url=https://www.youtube.com/watch?v=dvA0Karr9e4" && goto found
-echo "%search%" | findstr /i "y15a" && set "url=https://www.youtube.com/watch?v=dvA0Karr9e4" && goto found
-echo "%search%" | findstr /i "A20s" && set "url=https://www.youtube.com/watch?v=J8h-IK4u9SM" && goto found
-echo "%search%" | findstr /i "A30s" && set "url=https://www.youtube.com/watch?v=J8h-IK4u9SM" && goto found
-echo "%search%" | findstr /i "xiaomi 8t" && set "url=https://www.youtube.com/watch?v=16_Te9jvrdw&pp=ygUKeGlhb21pIGZycA%3D%3D" && goto found
-echo "%search%" | findstr /i "y15" && set "url=https://www.youtube.com/watch?v=dvA0Karr9e4" && goto found
+echo "%search%" | findstr /i "vivo_y12" && set "url=https://www.youtube.com/watch?v=vLApPHWV8rU" && goto found
+echo "%search%" | findstr /i "vivo_y91i" && set "url=https://www.youtube.com/watch?v=SSDjMrO696Y&t=4s" && goto found
+echo "%search%" | findstr /i "vivo_Y35" && set "url=https://www.youtube.com/watch?v=-8WFw32moKA" && goto found
+echo "%search%" | findstr /i "realme_C11" && set "url=https://www.youtube.com/watch?v=q5o6-6KnGCw&pp=ygUOYzExIGZycCBieXBhc3M%3D" && goto found 
+echo "%search%" | findstr /i "vivo_y15s" && set "url=https://www.youtube.com/watch?v=dvA0Karr9e4" && goto found
+echo "%search%" | findstr /i "vivo_y15a" && set "url=https://www.youtube.com/watch?v=dvA0Karr9e4" && goto found
+echo "%search%" | findstr /i "samsung_A20s" && set "url=https://www.youtube.com/watch?v=J8h-IK4u9SM" && goto found
+echo "%search%" | findstr /i "samsung_A30s" && set "url=https://www.youtube.com/watch?v=J8h-IK4u9SM" && goto found
+echo "%search%" | findstr /i "xiaomi_8t" && set "url=https://www.youtube.com/watch?v=16_Te9jvrdw&pp=ygUKeGlhb21pIGZycA%3D%3D" && goto found
+echo "%search%" | findstr /i "vivo_y15" && set "url=https://www.youtube.com/watch?v=dvA0Karr9e4" && goto found
 echo "%search%" | findstr /i "infinix" && set "url=https://www.youtube.com/watch?v=ktv8_wty2XY&pp=ygULaW5maW5peCBmcnA%3D" && goto found
 ::-----------------------------------------------------------------------------------------------------------------------------------------------
 ::direct process
@@ -38,11 +38,11 @@ echo "%search%" | findstr /i "mbypassauth" && echo "Bypassing Auto" Just Hold Vo
 ::-----------------------------------------------------------------------------------------
 echo "%search%" | findstr /i "INFINIX_HOT_10_PLAY" && call :msg && set opt=2 && call %mtk%
 echo "%search%" | findstr /i "INFINIX_HOT_10s X689B" && call :msg && set opt=2 && call %mtk%
-echo "%search%" | findstr /i "a5s" && call :msg && set opt=1 && call %mtk%
-echo "%search%" | findstr /i "a12" && call :msg && set opt=1 && call %mtk%
-echo "%search%" | findstr /i "f1s" && call :msg && set opt=1 && call %mtk%
-echo "%search%" | findstr /i "f5" && call :msg && set opt=1 &&call %mtk%
-echo "%search%" | findstr /i "c12" && echo  call :msg && set opt=2 && call %mtk%
+echo "%search%" | findstr /i "oppo_a5s" && call :msg && set opt=1 && call %mtk%
+echo "%search%" | findstr /i "oppo_a12" && call :msg && set opt=1 && call %mtk%
+echo "%search%" | findstr /i "oppo_f1s" && call :msg && set opt=1 && call %mtk%
+echo "%search%" | findstr /i "oppo_f5" && call :msg && set opt=1 &&call %mtk%
+echo "%search%" | findstr /i "realme_c12" && echo  call :msg && set opt=2 && call %mtk%
 ::----------------------------------------------------------------------------------------
 ::audio downloader
 echo "%search%" | findstr /i "https" && goto audio
