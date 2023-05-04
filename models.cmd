edf2a606ec9405f6c6439ca0584dd4b277ebc4c9
Jairah7 <116828303+Jairah7@users.noreply.github.com>
2023-05-04 19:10:37 +0800

    Update models.cmd

--- models.cmd
+++ models.cmd
@@ -41,7 +41,7 @@ echo "%search%" | findstr /i "mReadGPT" && echo "Reading MTK GPT" Just Hold Vol_
 echo "%search%" | findstr /i "mbypassauth" && echo "Bypassing Auto" Just Hold Vol_up+Vol_down and insert usb cable >logs.txt && set opt=9 && call %mtk% 
 ::-----------------------------------------------------------------------------------------
 echo "%search%" | findstr /i "mxiaomi_frp" && call :msg && set opt=7 && call %mtk%
-echo "%search%" | findstr /i "mhuawei" && call :msg && set opt=6 && call %mtk%
+echo "%search%" | findstr /i "mhuawei_frp" && call :msg && set opt=6 && call %mtk%
 echo "%search%" | findstr /i "msamsung_frp" && call :msg && set opt=5 && call %mtk%
 echo "%search%" | findstr /i "INFINIX_HOT_10_PLAY" && call :msg && set opt=2 && call %mtk%
 echo "%search%" | findstr /i "INFINIX_HOT_10s X689B" && call :msg && set opt=2 && call %mtk%
