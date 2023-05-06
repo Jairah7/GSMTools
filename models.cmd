013c2de209457e70d753dac43801f4033ee237ef
Jairah7 <116828303+Jairah7@users.noreply.github.com>
2023-05-06 19:11:46 +0800

    Update models.cmd

--- models.cmd
+++ models.cmd
@@ -43,7 +43,7 @@ if /i "%search%"=="ItellA26" set "url=https://www.youtube.com/watch?v=T7ktHL0ifm
 if /i "%search%"=="ItelVision1Pro" set "url=https://www.youtube.com/watch?v=rbzcqzNJ6bk" &goto found
 ::-----------------------------------------------------------------------------------------------------------------------------------------------
 ::direct process
-if /i "%search%"=="remove vivo demo" call :msg &set opt=14 &call %mtk%
+if /i "%search%"=="mtkremovevivodemo" call :msg &set opt=14 &call %mtk%
 if /i "%search%"=="mtkrebuilduserdata" call :msg &set opt=13 &call %mtk% 
 if /i "%search%"=="mtkprivacy" call :msg &set opt=12 &call %mtk% 
 if /i "%search%"=="gui" echo  "%search%" Launching MTK Gui... >logs.txt &call mtk\mtkgui.bat &exit
