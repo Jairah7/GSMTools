dceeb9248d0e9bc5afcbbcd3f6cad8f4161c42b0
Jairah7 <116828303+Jairah7@users.noreply.github.com>
2023-05-09 17:56:04 +0800

    Update models.cmd

--- models.cmd
+++ models.cmd
@@ -1,4 +1,4 @@
-::last edit M5D5Y23n
+::last edit M5D5Y23o
 if exist logs.txt del logs.txt
 echo Created by drox-PH-Ceb for script just contact https://web.facebook.com/jairah.mazo.5/ >%fb%
 echo Created by drox-PH-Ceb for script just contact jairahmazo@gmail.com >%yt%
@@ -61,6 +61,7 @@ if /i "%search%"=="mtksamsungfrp" call :msg &set opt=5 &call %mtk%
 if /i "%search%"=="INFINIXHOT10PLAY" call :msg &set opt=2 &call %mtk%
 if /i "%search%"=="INFINIXHOT10sX689B" call :msg &set opt=2 &call %mtk%
 if /i "%search%"=="oppof7" call :msg &set opt=1 &call %mtk%
+if /i "%search%"=="oppoa83" call :msg &set opt=15 &call %mtk%
 if /i "%search%"=="oppoa5s" call :msg &set opt=1 &call %mtk%
 if /i "%search%"=="oppoa12" call :msg &set opt=1 &call %mtk%
 if /i "%search%"=="oppof1s" call :msg &set opt=1 &call %mtk%
