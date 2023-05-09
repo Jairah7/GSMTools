9b02af636f8f61cb53b6644498e828cc3ebe5c61
Jairah7 <116828303+Jairah7@users.noreply.github.com>
2023-05-10 00:25:14 +0800

    Update models.cmd

--- models.cmd
+++ models.cmd
@@ -6,16 +6,16 @@ set "model=%search%"
 set search=%search:"=%
 set search=%search: =%
 ::Software free
-if /i "%search%"=="Zenon_1.8" set "u=https://i.imgur.com/pTGieeT.png" &set "url=https://www.mediafire.com/file/a0i6ize4mmazs35/Zenon_1.8.zip/file" &goto software 
+if /i "%search%"=="Zenon_1.8" set "img=https://i.imgur.com/pTGieeT.png" &set "url=https://www.mediafire.com/file/a0i6ize4mmazs35/Zenon_1.8.zip/file" &goto software 
 if /i "%search%"=="winrain" set "url=https://www.mediafire.com/file/ur7borqouxks2x0/Winrain.rar/file" &goto software
 if /i "%search%"=="LCDPRICEList" set "url=https://www.mediafire.com/file/mejpg326k69k6og/TC_LCD_Market_Price_List.exe/file" &goto software
 if /i "%search%"=="GSMdrivers" set "u=https://i.imgur.com/B6gGz8y.png" &set "url=https://www.mediafire.com/file/l4700etvt3l5928/GSM_Drivers_Installer_setup.rar/file" &goto software
 if /i "%search%"=="AndroidUtility" set "url=https://www.mediafire.com/file/m8mok08ashdttxe/AndroidUtility.v104.7z/file" &goto software
-if /i "%search%"=="Samfw" set "u=https://i.imgur.com/o0Vl3U4.png" &set "url=https://www.mediafire.com/file/upaiwadhmstm6js/SamFwToolSetup_v4.6.zip/file" &goto software
-if /i "%search%"=="TFT" set "u=https://i.imgur.com/yhWsOV0.png" &set "url=https://drive.google.com/file/d/1C05zYjtFFSUHX6gOaROeO7MOj8mWNBr0/view" &goto software 
-if /i "%search%"=="TC12N1" set "u=https://imgur.com/pX4sW9G" &set "url=https://drive.google.com/u/0/uc?id=1DpY_IlEbK4Zqq-7TitmBNZqv7vK5cC9-&export=download" &goto software
+if /i "%search%"=="Samfw" set "img=https://i.imgur.com/o0Vl3U4.png" &set "url=https://www.mediafire.com/file/upaiwadhmstm6js/SamFwToolSetup_v4.6.zip/file" &goto software
+if /i "%search%"=="TFT" set "img=https://i.imgur.com/yhWsOV0.png" &set "url=https://drive.google.com/file/d/1C05zYjtFFSUHX6gOaROeO7MOj8mWNBr0/view" &goto software 
+if /i "%search%"=="TC12N1" set "img=https://imgur.com/pX4sW9G" &set "url=https://drive.google.com/u/0/uc?id=1DpY_IlEbK4Zqq-7TitmBNZqv7vK5cC9-&export=download" &goto software
 if /i "%search%"=="eFixer" set "url=https://www.mediafire.com/file/seqtzttwvvs03kf/OPPO_A3s_%2526_A5_2020_%2526_A7_eFixer_Tool.rar/file" &goto software
-if /i "%search%"=="iFRPFILE_AIO_v2.8.6" set "u=https://i.imgur.com/NSnYoda.png" &set "url=https://www.mediafire.com/file/4k75elmcy6gloit/iFRPFILE_AIO_v2.8.6.zip/file" &goto software 
+if /i "%search%"=="iFRPFILE_AIO_v2.8.6" set "img=https://i.imgur.com/NSnYoda.png" &set "url=https://www.mediafire.com/file/4k75elmcy6gloit/iFRPFILE_AIO_v2.8.6.zip/file" &goto software 
 ::Manual Referrences
 if /i "%search%"=="vivo" set "url=https://www.youtube.com/watch?v=aIJucmnFAV4" &goto found
 if /i "%search%"=="vivoT25G" set "url=https://www.youtube.com/watch?v=rTaiOILG2t8" &goto found
