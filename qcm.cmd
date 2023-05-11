99d2f31bf49b6ed5149295f7a5dc07485138faeb
Jairah7 <116828303+Jairah7@users.noreply.github.com>
2023-05-11 16:00:31 +0800

    Update qcm.cmd

--- qcm.cmd
+++ qcm.cmd
@@ -1,13 +1,13 @@
-::last update M5D11Y23
+::last update M5D11Y23b
 if %opt%==1 goto sfrp
 
 :sfrp
 call :qport
 if not exist "%userprofile%\My Documents\Qualcomm_backup" mkdir "%userprofile%\My Documents\Qualcomm_backup"
 echo Please wait backing up persistent >>logs.txt
-emmcdl -p %port% -f "%mbn%" -d persistent "%userprofile%\My Documents\Qualcomm_backup"
+qcm\emmcdl -p %port% -f "%mbn%" -d persistent "%userprofile%\My Documents\Qualcomm_backup"
 echo Removing Samsung FRP...>>logs.txt
-emmcdl -p %port% -f "%mbn%" -e persistent >>logs.txt
+qcm\emmcdl -p %port% -f "%mbn%" -e persistent >>logs.txt
 goto exit 
 
 
@@ -22,14 +22,15 @@ reg query HKLM\hardware\devicemap\SERIALCOMM >%temp%\tmp
 findstr /I "QCUSB" %temp%\tmp >%temp%\tmp1 || goto sport
 for /f "tokens=3" %%G IN (%temp%\tmp1) DO set "port=%%G" &del /f %temp%\tmp1
 echo Device Found: %port% >>logs.txt
-echo Sending Loader... >>logs.txt &emmcdl -p %port% -f "%mbn%" >>logs.txt
+echo Sending Loader... >>logs.txt
+qcm\emmcdl -p %port% -f "%mbn%" >>logs.txt
 if %errorlevel% NEQ 0 ( goto rtrace ) else ( echo Sending Loader...OK >>logs.txt )
 exit /b
 
 
 :rtrace
 if %errorlevel%==0 ( echo Operation Finished. . .>>logs.txt) else (echo Operation Failed. . .>>logs.txt)
-if (%port%) NEQ () fhl --port=\\.\%port% --reset --noprompt --setactivepartition=0 >>logs.txt
+if (%port%) NEQ () qcm\\fhl --port=\\.\%port% --reset --noprompt --setactivepartition=0 >>logs.txt
 ::del /Q /A :H :S ( port_trace.txt bin\* ) >nul 2>&1 &set port=
 goto exit
 
