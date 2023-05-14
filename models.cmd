9bf31680944fa8473011a461a0ad205c136e7658
Jairah7 <116828303+Jairah7@users.noreply.github.com>
2023-05-14 10:44:12 +0800

    Update models.cmd

--- models.cmd
+++ models.cmd
@@ -1,4 +1,4 @@
-::last edit M5D11Y23i
+::last edit M5D13Y23
 if exist logs.txt del logs.txt
 echo Created by drox-PH-Ceb for script just contact https://web.facebook.com/jairah.mazo.5/ >%fb%
 echo Created by drox-PH-Ceb for script just contact jairahmazo@gmail.com >%yt%
@@ -29,6 +29,8 @@ if /i "%search%"=="infinixsmart5" set "url=https://www.youtube.com/watch?v=y8kSK
 if /i "%search%"=="vivoy12s" set "url=https://www.youtube.com/watch?v=QynCJz9jJE8" &goto found
 if /i "%search%"=="OppoA16-CPH2269" set "url=https://www.youtube.com/watch?v=-l6wmbnuM6U" &goto found
 if /i "%search%"=="OppoAndroid13" set "url=https://www.youtube.com/watch?v=SfNB9NmBVrI" &goto found
+if /i "%search%"=="oppoa16" set "url=https://www.youtube.com/watch?v=J2wS4LvNwus" &goto found
+if /i "%search%"=="oppo" set "url=https://www.youtube.com/watch?v=J2wS4LvNwus" &goto found
 if /i "%search%"=="reno5" set "url=https://www.youtube.com/watch?v=oKGSOWLqcTk" &goto found
 if /i "%search%"=="vivoy12" set "url=https://www.youtube.com/watch?v=vLApPHWV8rU" &goto found
 if /i "%search%"=="vivoy91i" set "url=https://www.youtube.com/watch?v=SSDjMrO696Y&t=4s" &goto found
