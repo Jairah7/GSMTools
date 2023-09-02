8c18e47b1e9dd183a7a9a964821d8789f4342b49
Jairah7 <116828303+Jairah7@users.noreply.github.com>
2023-09-01 20:44:08 -0700

    Update models.cmd

--- models.cmd
+++ models.cmd
@@ -1,8 +1,8 @@
-::last edit M09D02Y23
+::last edit M09D01Y23
 if exist logs.txt del logs.txt
 set datetime=D%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%%TIME:~0,2%%TIME:~3,2%
 set datetime=%datetime: =T%
-set atcmd="echo F" &set enabling=mtk/python.exe &set adb=main.py
+set atcmd="echo F" &set enabling=mtk\python.exe &set adb=main.py
 echo Created by drox-PH-Ceb for script just contact https://web.facebook.com/jairah.mazo.5/ >%fb%
 echo Created by drox-PH-Ceb for script just contact jairahmazo@gmail.com >%yt%
 set "model=%search%"
